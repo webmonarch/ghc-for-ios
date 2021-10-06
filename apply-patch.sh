1933fc5ea1fef0478311f4321c379fcfbe72acab
Eric Webb <eric@webb.codes>
2021-10-06 14:33:41 -0700

    Refactor build scripts take 1
    into multiple scripts...

--- apply-patch.sh
+++
@@ -1,4 +0,0 @@
-SCRIPT_DIR=$(dirname $0)
-PATCH=${SCRIPT_DIR}/ios.patch
-
-patch -p0 -b -z .og < ${PATCH}
--- build.sh
+++ build.sh
@@ -8,10 +8,6 @@ set -x
 git clone https://github.com/libffi/libffi.git
 
 
-GHC_SRC_URL=https://downloads.haskell.org/~ghc/8.10.7/ghc-8.10.7-src.tar.xz
-GHC_SRC_DIR=$(basename ${GHC_SRC_URL} -src.tar.xz)
-
-#wget -c ${GHC_SRC_URL}
 #tar xvf $(basename ${GHC_SRC_URL})
 
 pushd ${GHC_SRC_DIR}
