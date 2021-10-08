5485579f63fd6840b79a447c1204e87315b0a1a2
Eric Webb <eric@webb.codes>
2021-10-07 21:45:21 -0700

    Refactor build scripts take 2
    into a single file

--- _build_libffi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/libffi/libffi.git
-
-pushd libffi
-
-#xcodebuild -configuration Debug -target libffi-iOS -arch x86_64 -sdk iphonesimulator
-xcodebuild -configuration Release -target libffi-iOS -arch arm64 -sdk iphoneos
